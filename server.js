2b09351a63e30277d3d8bc3db461d4488827b5c1
dicristomanuel <manuel.dicristo@raise.com>
2016-07-12 08:57:19 -0500

    [server] set up routes

--- server.js
+++ server.js
@@ -37,8 +37,6 @@ server.register([
   if (err)
   throw err;
 
-  // main, auth & assets ====>
-
   server.route({
     method: 'GET',
     path: '/assets/{param*}',
@@ -51,120 +49,16 @@ server.register([
     }
   });
 
-  // server.route({
-  //   method: 'GET',
-  //   path: '/{path*}',
-  //   handler: (request, reply) => {
-  //     if (Object.keys(request.yar._store).length > 1)
-  //     reply.file('./public/index.html');
-  //     else
-  //     reply('Authentication Failed');
-  //   }
-  // });
-
-  server.route({ // temp route
+  server.route({
     method: 'GET',
     path: '/{path*}',
     handler: (request, reply) => {
       const { hash, name } = request.query;
-      Auth({ hash, name })
-      .then(() => {
-        request.yar.set({ hash, name });
-        reply.file('./public/index.html');
-      })
-      .catch(() => {
-        reply('No access permitted');
-      });
-    }
-  });
-
-  server.route({
-    method: 'POST',
-    path: '/member-service/login',
-    handler: (request, reply) => {
-      const { hash, name } = request.payload;
-      Auth({ hash, name })
-      .then(() => {
-        request.yar.set({ hash, name });
-        reply.redirect('/');
-      })
-      .catch(() => {
-        reply('No access permitted');
-        // redirect back to GC with error message
-      });
-    }
-  });
-
-  // message hooks ====>
-
-  server.route({
-    method: 'POST',
-    path: '/webhook',
-    handler(request, reply) {
-      const messaging_events = request.payload.entry[0].messaging;
-      for (let i = 0; i < messaging_events.length; i++) {
-        const event = request.payload.entry[0].messaging[i];
-        const sender = event.sender.id;
-        if (event.message) {
-          const text = event.message.text || event.message.attachments[0].payload.url;
-          Parser({io, sender, text, userType: Consumer});
-        }
-      }
-      reply();
-    }
-  });
-
-  server.route({
-    method: 'POST',
-    path: '/member-service',
-    handler(request, reply) {
-      const { chatId, text } = request.payload;
-      Parser({ chatId, text, io, userType: MemberService });
-      reply();
-    }
-  });
-
-  // helpers ====>
-
-  server.route({
-    method: 'GET',
-    path: '/get-chats',
-    handler(request, reply) {
-      const msAuth = request.yar._store;
-      GetChats(msAuth).then((chats) => {
-        reply({ allChats: chats[0], engagedChats: chats[1], msAuth })
-      });
-    }
-  });
-
-  server.route({
-    method: 'PUT',
-    path: '/update-chat',
-    config: {
-      validate: {
-        payload: {
-          chatId: Joi.number().required(),
-          key: Joi.required(),
-          value: Joi.required(),
-        },
-      },
-    },
-    handler(request, reply) {
-      UpdateStatus(io, request.payload);
-      reply();
-    }
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/get-messages',
-    handler(request, reply) {
-      const { id, page } = request.query;
-      GetMessages(id, page).then(reply);
+      request.yar.set({ hash, name });
+      reply.file('./public/index.html');
     }
   });
 });
-
 
 server.start((error) => {
   if (error) {
